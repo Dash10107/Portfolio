0053023f93934e7346df25ff1aeaada978d72a7b
Dash10107 <dakshcjain@gmail.com>
2024-06-02 17:40:38 +0530

    commited till created proper portfolio

---
+++ constants/data.js
@@ -0,0 +1,117 @@
+const userData = {
+  githubUsername: "Dash10107",
+  name: "Daksh Jain",
+  designation: "Full-Stack Developer || Machine Learning",
+  avatarUrl: "/avatar.jpg",
+  email: "dakshcjain@gmail.com",
+  phone: "+91 7977113766",
+  address: "Mumbai, Maharashtra, India.",
+  projects: [
+    {
+      title: "EduSync",
+      link: "https://edusync.netlify.app/",
+      imgUrl: "/edusync.png",
+    },
+    {
+      title: "Cypress",
+      link: "https://cypress-d.azurewebsites.net/",
+      imgUrl: "/notion-cypress.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://localhost:3000",
+      imgUrl: "/portfolio.png",
+    },
+
+    {
+      title: "Customer Dashboard",
+      link: "https://nextjs-dashboard-theta-smoky-12.vercel.app/",
+      imgUrl: "/dashboard.png",
+      
+    },
+    {
+      title: "Imagi Nation",
+      link: "https://stunning-yeot-f94b6f.netlify.app/",
+      imgUrl: "/imagination.png",
+    },
+        {
+      title: "IPL Win Predictor",
+      link: "https://ipl-win-predictor-1vm5.onrender.com/",
+      imgUrl: "/ipl-win.png",
+    },
+
+    {
+      title: "DSurv-Token",
+      link: "https://gn3qs-vqaaa-aaaam-ab7fa-cai.icp0.io/",
+      imgUrl: "/dsurv.png",
+    },
+    {
+      title: "Secrets Storing App",
+      link: "https://secrets-340n.onrender.com/",
+      imgUrl: "/secrets.png",
+    },
+    {
+      title: "Quote Generator",
+      link: "https://remarkable-phoenix-332081.netlify.app/",
+      imgUrl: "/quote.png",
+    },
+    {
+      title: "Book Recommender",
+      link: "https://book-recommender.azurewebsites.net/",
+      imgUrl: "/book-recommender.png",
+    },
+
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I'am 19 years old and budding computer engineer, `,
+      `My passion for computer science ignited while learning HTML and C language in high school. Witnessing code execution developed my interest, and the freedom to creatively solve problems appealed to me. Programming isn't just a skill; it's an aid to simplify lives and aid others.`,
+
+      `During a 4-month internship as a Frontend Developer in Innovations Hacks AI, A Pune-based Startup Company, I acquired valuable skills in web development, user interface design, and front-end technologies. It helped me build a comprehensive understanding of the software development life cycle. My practical experience as a front-end and full-stack developer has been instrumental in shaping my passion for computer science. `,
+    ],
+    // currentProject: "Tailwind Master Kit",
+    // currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+
+
+    {
+      title: " Frontend Developer , Internship",
+      company: "Innovation Hacks AI",
+      year: "2023",
+      companyLink: "https://www.innovation-hacks.ai/",
+      desc: "Spearheaded frontend development for Datawise, an innovative AI-based Data Management System, driving user-centric design and functionality.",
+    },
+    {
+      title: "Diploma",
+      company: "KJ Somaiya Polytechnic",
+      year: "2021-2024",
+      companyLink: "https://polytechnic.somaiya.edu.in/polytechnic",
+      desc: "Diploma in Computer Science and Engineering. Last Semester Grade was 95.6%.",
+    },
+    {
+      title: "High School",
+      company: "SKI  High School, Mumbai.",
+      year: "2011-2021",
+      // companyLink: "https://upes.co.in",
+      desc: "Completed my 10th Standard with 91.6%.",
+    },
+  ],
+  resumeUrl:
+    "https://docs.google.com/document/d/1-FEHArQSL9wUIMftqTcy39VOeEHrAGjRrlPai7Fk20A/edit?usp=sharing",
+  socialLinks: {
+    hackerrank: "https://www.hackerrank.com/profile/dakshcjain",
+    twitter: "https://x.com/Daksh10107",
+    linkedin: "https://www.linkedin.com/in/daksh-jain-556b78212",
+    instagram: "https://github.com/Dash10107/",
+    github:"https://github.com/Dash10107/",
+    leetcode: "https://leetcode.com/Dash10107/",
+    peerlist: "https://peerlist.io/dakshjain",
+  },
+};
+
+export default userData;
+
+
